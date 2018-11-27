aff2ba6c240bddeae120a315fb2de7fe988bc07c
Dionisio Perez-Mavrogenis <dperezmavrogenis@lastmilelink.com>
2018-11-27 15:12:37 +0000

    added version as a const

--- util/util.go
+++ util/util.go
@@ -7,12 +7,16 @@ import (
 	"fmt"
 )
 
+const (
+	slackApiVersion = "v0"
+)
+
 func BuildMessageForAuth(body, timestamp string, key []byte) []byte {
 	return []byte(BuildMessageForAuthString(body, timestamp, key))
 }
 
 func BuildMessageForAuthString(body, timestamp string, key []byte) string {
-	message := fmt.Sprintf("v0:%s:%s", timestamp, body)
+	message := fmt.Sprintf("%s:%s:%s", slackApiVersion, timestamp, body)
 	mac := hmac.New(sha256.New, key)
 	mac.Write([]byte(message))
 	return fmt.Sprintf("v0=%s", hex.EncodeToString(mac.Sum(nil)))
